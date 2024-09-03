b2d165fa50b86ef79092f70ad4f9c6d01395b18a
Benny <benjamin.olivaressiilva@elev.ga.ntig.se>
2024-09-03 12:36:57 +0200

    kuyrkuyr

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+Console.WriteLine("Hur lång är du?");
+string längd = Console.ReadLine();
+
+int längdInt = 0;
+
+int.TryParse(längd, out längdInt);
+
+if (längdInt < 160)
+{
+Console.WriteLine("Du är kort");
+}
+else 
+Console.WriteLine("du är lång");
+Console.ReadLine();
